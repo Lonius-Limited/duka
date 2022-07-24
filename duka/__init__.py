9d7cb4cfb81e24a87d9d434e3433f0ad0fc17f95
Baba Zaha <dsmwaura@gmail.com>
2022-07-24 15:35:40 +0300

    Duka workspace

--- duka/__init__.py
+++ duka/__init__.py
@@ -1,3 +1,11 @@
 
 __version__ = '0.0.1'
+import frappe
+from frappe import _
+
+def execute_duka_workspaces():
+    exclusion_list = ('Master Data','Accounting','Stock','Manufacturing','POS Awesome','Buying')
+    print(_(exclusion_list))
+    frappe.db.sql("DELETE FROM `tabWorkspace` WHERE name NOT IN {}".format(exclusion_list))
+    print("Deleted unnecesary workspaces for Duka")
 
