b761df955c2780de6b840bff79c3eb7c324d4bc0
Baba Zaha <dsmwaura@gmail.com>
2022-08-08 14:37:16 +0300

    Subscriptable object error on duka

--- duka/api/realtime.py
+++ duka/api/realtime.py
@@ -2,4 +2,9 @@ import frappe
 
 
 def check():
-    frappe.publish_realtime(event='msgprint', message='Hello Socketio', user="Administrator")
+    # frappe.publish_realtime('msgprint', message='Hello Socketio')
+    # doctype, docname = "User", "cashier@lonius.co.ke"
+    # description = "Hello shifty"
+    # frappe.publish_progress(10, title=None, doctype=None, docname=None, description=None)
+    msg_var= "Hey baby"
+    frappe.publish_realtime(event='eval_js', message='alert("{0}")'.format(msg_var), user="Administrator")
