4c95eab593c3ee195f43dda9b9cbecf5a7fe9523
SALIM MWAURA <dsmwaura@gmail.com>
2023-08-28 23:10:12 +0300

    Update realtime.py

--- duka/api/realtime.py
+++ duka/api/realtime.py
@@ -6,5 +6,5 @@ def check():
     # doctype, docname = "User", "cashier@lonius.co.ke"
     # description = "Hello shifty"
     # frappe.publish_progress(10, title=None, doctype=None, docname=None, description=None)
-    msg_var= "Hey baby"
+    msg_var= "Hey little baby"
     frappe.publish_realtime(event='eval_js', message='alert("{0}")'.format(msg_var), user="Administrator")
